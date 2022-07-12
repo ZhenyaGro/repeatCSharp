2bbaa136e8d40d20dce7d53bb5385b44b2f490d7
ZhenyaGro <bon4ios@gmail.com>
2022-07-12 23:05:18 +0300

    Tasks of seminar 2 in progress...

--- Program.cs
+++ Program.cs
@@ -3,8 +3,19 @@
 // Start the app and propose to choose the task
 static void selectLesson()
 {
-  Console.WriteLine("На данный момент доступны только задания первого семинара.");
-  Console.WriteLine();
+  Console.WriteLine("Доступны задания первого и второго семинаров.\nВведите номер семинара запускаемых заданий (цифрой 1 или 2)");
+  switch (Console.ReadLine())
+  {
+    case "1":
+      Seminar1.TaskSelector.launchTask();
+      break;
 
-  Seminar1.TaskSelector.launchTask();
+    case "2":
+      Seminar2.TaskSelector.launchTask();
+      break;
+
+    default:
+      Console.WriteLine("Некорректный ввод");
+      break;
+  }
 }
---
+++ seminars/seminar2.cs
@@ -0,0 +1,79 @@
+﻿namespace Seminar2
+{
+  // Start the app and propose to choose the task
+  class TaskSelector
+  {
+    public static void launchTask()
+    {
+      Console.Write("Выберите задание второго семинара (доступно: 10, 13, 15): ");
+      switch (Console.ReadLine())
+      {
+        case "10":
+          Console.WriteLine();
+          Tasks.task10();
+          break;
+
+        case "13":
+          Console.WriteLine();
+          Tasks.task13();
+          break;
+
+        case "15":
+          Console.WriteLine();
+          Tasks.task15();
+          break;
+
+        default:
+          Console.WriteLine("Задания с таким номером нет.");
+          break;
+      }
+    }
+  }
+  class Tasks
+  {
+    public static void task10()
+    {
+      /*
+      Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+      456 -> 5
+      782 -> 8
+      918 -> 1
+      */
+      Console.WriteLine("Запущено задание 10. Вывод второй цифры трехзначного числа");
+      Console.Write("Введите трехзначное число: ");
+      string userAnswer = Console.ReadLine();
+
+      if (userAnswer.Length != 3)
+      {
+        Console.WriteLine("Некорректный ввод");
+        return;
+      }
+
+      int[] numbers = new int[3];
+      for (int i = 0; i < 3; i++)
+      {
+        numbers[i] = Convert.ToInt32(userAnswer.Split(""));
+      }
+
+      Console.WriteLine(numbers[2]);
+    }
+    public static void task13()
+    {
+      /*
+      Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+      645 -> 5
+      78 -> третьей цифры нет
+      32679 -> 6
+      */
+    }
+    public static void task15()
+    {
+      /*
+      Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+      6 -> да
+      7 -> да
+      1 -> нет
+      */
+    }
+  }
+}
