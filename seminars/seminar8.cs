494a8b3393a983fe6739188748812d41d02657ea
ZhenyaGro <bon4ios@gmail.com>
2022-08-22 21:49:56 +0300

    Tasks 54 and 56

--- seminars/seminar8.cs
+++ seminars/seminar8.cs
@@ -14,7 +14,7 @@ class Seminar8 : BaseSeminar
   public static void Task54()
   {
     /*
-    Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+    Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по ?убыванию? (возрастанию?) элементы каждой строки двумерного массива.
     Например, задан массив:
     1 4 7 2
     5 9 2 3
@@ -24,8 +24,61 @@ class Seminar8 : BaseSeminar
     2 3 5 9
     2 4 4 8
     */
-    Console.WriteLine("Запущено задание 54. Элементы по убыванию в каждой строке двумерного массива");
+    Console.WriteLine("Запущено задание 54. Элементы по возрастанию в каждой строке двумерного массива");
 
+    // CreateArray();
+    /*
+    int[,] numbers = new int[3, 4]{
+      {1, 4, 7, 2},
+      {5, 9, 2, 3},
+      {8, 4, 2, 4}};
+    */
+
+    int[,] numbers = createArray2Dimensional();
+    ShowArray2Dimensional(numbers);
+    Console.WriteLine("");
+    Console.Write("----------------");
+    BubbleSort(numbers);
+    ShowArray2Dimensional(numbers);
+
+    static int[,] createArray2Dimensional()
+    {
+      Random rnd = new Random();
+      int countRow = rnd.Next(3, 10);
+      int countCol = rnd.Next(4, 10);
+      int[,] numbers = new int[countRow, countCol];
+
+      for (int i = 0; i < countRow; i++)
+      {
+        for (int j = 0; j < countCol; j++)
+        {
+          numbers[i, j] = rnd.Next(0, 10);
+        }
+      }
+      return numbers;
+    }
+
+    static void BubbleSort(int[,] array)
+    {
+      int countRows = array.GetLength(0);
+      int countColumns = array.GetLength(1);
+
+      for (int n = 0; n < countRows; n++)
+      {
+        for (int i = 0; i < countColumns; i++)
+        {
+          for (int j = 0; j < countColumns - 1 - i; j++)
+          {
+            if (array[n, j] > array[n, j + 1])
+            {
+              int temp = array[n, j];
+              array[n, j] = array[n, j + 1];
+              array[n, j + 1] = temp;
+            }
+          }
+        }
+      }
+    }
   }
   public static void Task56()
   {
@@ -40,6 +93,55 @@ class Seminar8 : BaseSeminar
     */
     Console.WriteLine("Запущено задание 56. Строка с наименьшей суммой элементов");
 
+    int[,] numbers = createArray2Dimensional();
+    ShowArray2Dimensional(numbers);
+    Console.WriteLine();
+    Console.WriteLine($"{SearchMinSum(numbers)} строка?");
+
+
+    static int[,] createArray2Dimensional()
+    {
+      Random rnd = new Random();
+      int countEl = rnd.Next(3, 4);
+      int[,] numbers = new int[countEl, countEl];
+
+      for (int i = 0; i < countEl; i++)
+      {
+        for (int j = 0; j < countEl; j++)
+        {
+          numbers[i, j] = rnd.Next(0, 10);
+        }
+      }
+      return numbers;
+    }
+
+    int SearchMinSum(int[,] array)
+    {
+      int[] sums = new int[array.GetLength(0)];
+
+      for (int i = 0; i < array.GetLength(0); i++)
+      {
+        int sum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          sum += array[i, j];
+        }
+        sums[i] = sum;
+      }
+
+      int result = sums[0];
+      int min = -1;
+      for (int i = 0; i < sums.Length; i++)
+      {
+        if (result > sums[i])
+        {
+          min = i + 1;
+          result = sums[i];
+        }
+      }
+
+      return min;
+    }
   }
   public static void Task58()
   {
@@ -84,5 +186,16 @@ class Seminar8 : BaseSeminar
     10 9 8 7
     */
     Console.WriteLine("Запущено задание 62. Спиральное заполнение");
+  }
+  static void ShowArray2Dimensional(int[,] array)
+  {
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      Console.WriteLine();
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+        Console.Write($"{array[i, j]} ");
+      }
+    }
   }
 }
